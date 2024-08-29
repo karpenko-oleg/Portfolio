e4c89106b5f249f6a38441552a106ec751835428
Oleg <116677837+karpenko-oleg@users.noreply.github.com>
2024-08-29 18:57:46 +0700

    Delete js directory

--- js/app.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.header__btn').addEventListener('click', function () {
-    this.classList.toggle('active');
-    document.querySelector('.header__nav').classList.toggle('open');
-  });
